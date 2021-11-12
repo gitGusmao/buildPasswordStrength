7e3d5195ffd117bb1f3de66407e31987ff3ee861
Leonardo Gusmão <gusmao.on@gmail.com>
2021-11-12 16:11:00 -0300

    clear

--- esm2015/lib/enum/colors.enum.js
+++
@@ -1,7 +0,0 @@
-export var Colors;
-(function (Colors) {
-    Colors["primary"] = "primary";
-    Colors["accent"] = "accent";
-    Colors["warn"] = "warn";
-})(Colors || (Colors = {}));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29sb3JzLmVudW0uanMiLCJzb3VyY2VSb290IjoiLi4vLi4vLi4vLi4vcHJvamVjdHMvYW5ndWxhci1tYXRlcmlhbC1leHRlbnNpb25zL3Bhc3N3b3JkLXN0cmVuZ3RoL3NyYy8iLCJzb3VyY2VzIjpbImxpYi9lbnVtL2NvbG9ycy5lbnVtLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLE1BQU0sQ0FBTixJQUFZLE1BSVg7QUFKRCxXQUFZLE1BQU07SUFDaEIsNkJBQW1CLENBQUE7SUFDbkIsMkJBQWlCLENBQUE7SUFDakIsdUJBQWEsQ0FBQTtBQUNmLENBQUMsRUFKVyxNQUFNLEtBQU4sTUFBTSxRQUlqQiIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBlbnVtIENvbG9ycyB7XHJcbiAgcHJpbWFyeSA9ICdwcmltYXJ5JyxcclxuICBhY2NlbnQgPSAnYWNjZW50JyxcclxuICB3YXJuID0gJ3dhcm4nXHJcbn1cclxuIl19
--- schematics/helpers/material/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './ast/ng-module-imports';
-export * from './build-component';
-export * from './package-config';
